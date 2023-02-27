acd2fae09a6503db9d527d96a27a9b82ab1d6a37
Zrzi <201900301041@mail.sdu.edu.cn>
2023-02-27 12:50:38 +0800

    '添加获取导航菜单接口'

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -8,7 +8,9 @@
         <el-icon><Location /></el-icon>
         <span>{{ system.systemName}}</span>
       </template>
-      <el-menu-item v-for="menu in system.menus" :index="menu.path" @click="handleClick">
+      <el-menu-item v-for="menu in system.menus"
+                    :index="menu.path"
+                    @click="handleClick" >
         {{ menu.menuName }}
       </el-menu-item>
     </el-sub-menu>
@@ -20,41 +22,53 @@ export default {
   name: "SideMenu",
   data() {
     return {
-      systems: [
-        {
-          systemId: 'S01',
-          systemName: '权限管理',
-          menus: [
-            {
-              menuId: 'M01',
-              menuName: '用户管理',
-              path: '/home/users'
-            },
-            {
-              menuId: 'M02',
-              menuName: '角色管理',
-              path: '/home/roles'
-            },
-            {
-              menuId: 'M03',
-              menuName: '系统管理',
-              path: '/home/systems'
-            }
-          ]
-        },
-        {
-          systemId: 'S02',
-          systemName: 'Excel映射管理',
-          menus: []
-        }
-      ]
+      systems: null
+      // systems: [
+      //   {
+      //     systemId: 'S01',
+      //     systemName: '权限管理',
+      //     menus: [
+      //       {
+      //         menuId: 'Rfd7d453ce918403ab0ccc4b7d00e693',
+      //         menuName: '用户管理',
+      //         path: '/home/users'
+      //       },
+      //       {
+      //         menuId: 'Rfb45f4d2b0ce4751a66a892cfbc6ec96',
+      //         menuName: '角色管理',
+      //         path: '/home/roles'
+      //       },
+      //       {
+      //         menuId: 'Re6654c09a55943ba8e8c63826fdc7a26',
+      //         menuName: '系统管理',
+      //         path: '/home/systems'
+      //       }
+      //     ]
+      //   },
+      //   {
+      //     systemId: 'S02',
+      //     systemName: 'Excel映射管理',
+      //     menus: []
+      //   }
+      // ]
     }
   },
   methods: {
     handleClick(value) {
       let path = value.index;
       this.$router.push(path);
+    },
+    initSystems() {
+      let _this = this;
+      _this.$httpAuthority.get('/getMenus').then(res => {
+        const response = res.data;
+        console.log(response);
+        _this.systems = response.data;
+      });
     }
+  },
+  created() {
+    this.initSystems();
   }
 }
 </script>
