5988ce36499a75dbd0a1ad7509bff4cd8da22198
Zrzi <201900301041@mail.sdu.edu.cn>
2023-03-07 15:39:07 +0800

    '上传Excel前端完善'

--- src/components/dialog/excels/UploadExcelDialog.vue
+++ src/components/dialog/excels/UploadExcelDialog.vue
@@ -1,6 +1,11 @@
 <template>
-  <el-dialog v-model="uploadExcelFormVisible" title="上传Excel文件" @opened="init" @close="cancelUploadExcel">
-    <el-form style="text-align: left">
+  <el-dialog
+      v-model="uploadExcelFormVisible"
+      title="上传Excel文件"
+      v-loading="loading"
+      @opened="init"
+      @close="cancelUploadExcel">
+    <el-form style="text-align: left" v-show="show">
       <el-upload
           v-model:file-list="fileList"
           accept=".xls, .xlsx"
@@ -25,11 +30,13 @@
 <!--        </template>-->
       </el-upload>
     </el-form>
-<!--    <template #footer>-->
-<!--        <span class="dialog-footer">-->
-<!--          <el-button type="primary" @click="uploadExcel">上传文件</el-button>-->
-<!--        </span>-->
-<!--    </template>-->
+    <el-scrollbar v-show="!show">
+      <el-row v-for="message in errorMessages">
+        <el-col :span="2"></el-col>
+        <el-col :span="20">{{ message }}</el-col>
+        <el-col :span="2"></el-col>
+      </el-row>
+    </el-scrollbar>
   </el-dialog>
 </template>
 
@@ -42,7 +49,11 @@ export default {
     return {
       uploadExcelFormVisible: false,
       fileList: [],
-      uploadExcelFormRules: {}
+      uploadExcelFormRules: {},
+      show: true,
+      loading: false,
+      taskId: '',
+      errorMessages: []
     }
   },
   methods: {
@@ -90,16 +101,41 @@ export default {
       this.forms.append('file', file);
       let _this = this;
       _this.$httpAuthority.post('/excel/upload', this.forms, {headers: {'Content-Type': 'multipart/form-data'}}).then(res => {
-        ElMessage({
-          message: '文件上传成功',
-          duration: 3 * 1000,
-          center: true,
-          type: 'success'
-        });
-        _this.cancelUploadExcel();
+        const result = res.data;
+        _this.taskId = result.data;
+        _this.show = false;
+        _this.loading = true;
+        _this.getUploadExcelResult();
       }).catch(message => {
         _this.fileList = [];
       });
+    },
+    getUploadExcelResult() {
+      let _this = this;
+      let taskId = _this.taskId;
+      let interval = setInterval(() => {
+        _this.$httpAuthority.get('/excel/getUploadExcelResult', {params: {taskId}}).then(res => {
+          const result = res.data;
+          const status = result.data.taskStatus;
+          if (status === 1) {
+            // 任务成功且没有异常信息
+            _this.loading = true;
+            clearInterval(interval);
+            ElMessage({
+              message: '文件上传成功',
+              duration: 3 * 1000,
+              center: true,
+              type: 'success'
+            });
+          } else if (status === 2) {
+            // 任务完成且有异常信息
+            _this.loading = true;
+            clearInterval(interval);
+            _this.errorMessages = result.data.errorMessage;
+          }
+        });
+      }, 1000);
+      this.fileList = [];
     }
   }
 }
